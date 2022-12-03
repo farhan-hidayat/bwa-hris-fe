e7f429edb40e3dd24e8fa8f6bdc8f541af0ff2e2
Farhan Hidayat Al Rachman <55468685+farhan-hidayat@users.noreply.github.com>
2022-12-03 17:15:10 +0700

    update link api

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://bwa-hris.test/api/',
+    baseURL: 'https://syailendra-app.my.id/api/',
   },
 
   auth: {
